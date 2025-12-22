2245fbe3367d705b6135865313a4d2d6e560d67a
tajul islam <tajulislam199595@gmail.com>
2025-12-22 17:23:54 +0600

    third-commit

--- queries.sql
+++ queries.sql
@@ -40,5 +40,43 @@ create table bookings(
 
 -- Retrieve booking information along with Customer name and Vehicle name.
 
+select
+  b.booking_id,
+  u.name as customer_name,
+  v.name as vehicle_name,
+  b.start_date,
+  b.end_date,
+  b.status
+from
+  bookings b
+  join users u on u.user_id = b.user_id
+  join vehicles v on v.vehicle_id = b.vehicle_id;
+
+-- Find all vehicles that have never been booked.
+select
+  * from vehicles v
+where
+  not exists (
+    select
+      *
+    from
+      bookings b
+    where
+      b.vehicle_id = v.vehicle_id
+  );
 
 
+  --  Retrieve all available vehicles of a specific type (e.g. cars).
+
+  select * from vehicles where status='available' and type='car';
+
+  -- Find the total number of bookings for each vehicle and display only those vehicles that have more than 2 bookings.
+
+  select
+  v.name as vehicle_name,
+  count(b.vehicle_id) as total_bookings
+from
+  bookings b
+  join vehicles v on v.vehicle_id = b.vehicle_id
+group by
+  v.name having count(b.vehicle_id)>2;
